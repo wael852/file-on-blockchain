1a6d57bf2a86bd44c3cd5bc2998da063c9d31beb
wael852 <waelmej@gmail.com>
2022-05-29 17:35:52 +0100

    updated deprecated rpc to http

--- blockchain/start.sh
+++ blockchain/start.sh
@@ -1,1 +1,1 @@
-geth --rpc --rpccorsdomain "*" --nodiscover --datadir "chain-data" --port "30303" --rpcapi "db,eth,net,web3,personal,admin,miner,debug,txpool" --networkid 1001012
+geth --http --http.corsdomain "*" --nodiscover --datadir "chain-data" --http.port "8081" --http.api "db,eth,net,web3,personal,admin,miner,debug,txpool" --networkid 1001012
